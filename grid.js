79e7162c6d301fa2343f3cd2bf8f376075347c0e
Timur Çelikel <me@timurcelikel.com>
2015-12-21 00:45:38 +0100

    feat(grid): add states and set function

--- grid.js
+++ grid.js
@@ -2,6 +2,17 @@
 
 const CLASS_NAME = 'rounded-grid';
 
+const SHAPES = [
+	'blank',
+	'square',
+	'tr',
+	'br',
+	'bl',
+	'tl',
+	'circle', 
+	'rotated'
+];
+
 exports.grid = class Grid {
 	constructor(rootElement, width, height) {
 		this.gridArray = [];
@@ -10,24 +21,40 @@ exports.grid = class Grid {
 
 		rootElement.classList.add(CLASS_NAME);
 
-		for (var rowIndex = 0; rowIndex < width; rowIndex++) {
+		for (let rowIndex = 0; rowIndex < width; rowIndex++) {
 			if (!this.gridArray[rowIndex]) {
 				this.gridArray[rowIndex] = [];
 			};
 
-			let rowElement = _addElement(rootElement, CLASS_NAME + '_row');
+			let rowElement = this._addElement(rootElement, CLASS_NAME + '_row');
 
 			for (let columnIndex = 0; columnIndex < height; columnIndex++) {
-				let columnElement = _addElement(rowElement, CLASS_NAME + '_cell');
+				let columnElement = this._addElement(rowElement);
+				this._setCell(columnElement, 0)
 				this.gridArray[rowIndex].push(columnElement)
 			}
 		}
+	}
 
-		function _addElement(rootElement, className) {
-			let element = document.createElement('div');
+	_addElement(rootElement, className) {
+		let element = document.createElement('div');
+		if (className) {
 			element.classList.add(className);
-			rootElement.appendChild(element);
-			return element;
-		};
+		}
+		rootElement.appendChild(element);
+		return element;
+	};
+
+	_setCell(cell, state) {
+		if (cell.dataset.roundedGridState === state) {
+			return;
+		}
+
+		cell.className = this._generateStateClassName(state);
+		cell.dataset.roundedGridState = state;
+	}
+
+	_generateStateClassName(state) {
+		return CLASS_NAME + '_cell ' + CLASS_NAME + '_cell-' + SHAPES[state];
 	}
 }
