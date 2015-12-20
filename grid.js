5e041f753ae079715267a70a7ed81cbf23e1685c
Timur Çelikel <me@timurcelikel.com>
2015-12-20 16:05:28 +0100

    feat(grid): basic styling

--- grid.js
+++ grid.js
@@ -1,20 +1,24 @@
 'use strict';
 
+const CLASS_NAME = 'rounded-grid';
+
 exports.grid = class Grid {
 	constructor(rootElement, width, height) {
 		this.gridArray = [];
 		this.width = width;
 		this.height = height;
 
+		rootElement.classList.add(CLASS_NAME);
+
 		for (var rowIndex = 0; rowIndex < width; rowIndex++) {
 			if (!this.gridArray[rowIndex]) {
 				this.gridArray[rowIndex] = [];
 			};
 
-			let rowElement = _addElement(rootElement, 'row_' + rowIndex);
+			let rowElement = _addElement(rootElement, CLASS_NAME + '_row');
 
 			for (let columnIndex = 0; columnIndex < height; columnIndex++) {
-				let columnElement = _addElement(rowElement, 'column_' + columnIndex);
+				let columnElement = _addElement(rowElement, CLASS_NAME + '_cell');
 				this.gridArray[rowIndex].push(columnElement)
 			}
 		}
