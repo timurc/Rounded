eafad1adb8c0c727630ef2c3f068e1585e4516da
Timur Çelikel <me@timurcelikel.com>
2016-01-02 22:29:35 +0100

    feat(script include): include with defer, remove contentLoaded

--- main.js
+++ main.js
@@ -4,6 +4,4 @@ require("./style.less");
 
 let characters = require('./characters.js').getCharacters();
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-	let grid = new Grid(document.getElementById('Grid-1'), 50, 50, true);
-});
+let grid = new Grid(document.getElementById('Grid-1'), 50, 50, true);
