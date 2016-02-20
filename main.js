29172997df4dd3a6248a08471fac870616bb2fd6
Timur Çelikel <me@timurcelikel.com>
2016-02-20 20:16:28 +0100

    fead(rounded) move less file, add second test grid

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 import Grid from 'grid';
 
+require("./rounded.less");
 require("./style.less");
 
 let characters = require('./characters.js').getCharacters();
 
 window.grid = new Grid(document.getElementById('Grid-1'));
+
+window.grid2 = new Grid(document.getElementById('Grid-2'), {
+	data: [
+		[1,1,3,0,0,0,0,6,0,5],
+		[1,3,0,0,0,0,6,0,5,3],
+		[3,0,0,0,0,6,0,5,3,0],
+		[0,0,0,0,6,0,5,3,0,6],
+		[0,0,0,6,0,5,3,0,6,0],
+		[0,0,6,0,5,3,0,6,0,0],
+		[0,6,0,5,3,0,6,0,0,0],
+		[6,0,5,3,0,6,0,0,0,5],
+		[0,5,3,0,6,0,0,0,5,1],
+		[5,3,0,6,0,0,0,5,1,1]
+	]
+});
