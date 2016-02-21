f77af3d9c8aefad32952660e3db4debaea37ad56
Timur Çelikel <me@timurcelikel.com>
2016-02-21 14:39:49 +0100

    feat(grid.js): move defaults to separate file

---
+++ defaults.js
@@ -0,0 +1,31 @@
+export default {
+	CLASS_NAME: 'rounded-grid',
+	SHAPES: [
+		'blank',
+		'square',
+		'tr',
+		'br',
+		'bl',
+		'tl',
+		'circle', 
+		'rotated'
+	],
+	KEYMAP: {
+		1: 'tr',
+		2: 'tl',
+		3: 'bl',
+		4: 'br',
+		5: 'circle',
+		6: 'square',
+		7: 'blank'
+	}, 
+	isEditable: true,
+	data: [
+		[0,0,0,0,0,0],
+		[0,0,0,0,0,0],
+		[0,0,0,0,0,0],
+		[0,0,0,0,0,0],
+		[0,0,0,0,0,0],
+		[0,0,0,0,0,0]
+	]
+};
--- grid.js
+++ grid.js
@@ -1,35 +1,4 @@
-
-const DEFAULTS = {
-	CLASS_NAME: 'rounded-grid',
-	SHAPES: [
-		'blank',
-		'square',
-		'tr',
-		'br',
-		'bl',
-		'tl',
-		'circle', 
-		'rotated'
-	],
-	KEYMAP: {
-		1: 'tr',
-		2: 'tl',
-		3: 'bl',
-		4: 'br',
-		5: 'circle',
-		6: 'square',
-		7: 'blank'
-	}, 
-	isEditable: true,
-	data: [
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0]
-	]
-};
+import DEFAULTS from 'defaults';
 
 export default class Grid {
 	constructor(rootElement, config) {
