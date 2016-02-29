f38903d1fd3d3ed614761321730326ba9ec3c82f
Timur Çelikel <me@timurcelikel.com>
2016-02-29 18:25:29 +0100

    feat(type-example): list container

--- examples/type-edit.js
+++ examples/type-edit.js
@@ -6,6 +6,8 @@ require("examples/style.less");
 
 const headings = document.querySelectorAll('h2');
 
+const typeEditList = document.getElementById('typeList');
+
 for (let i = 0; i < headings.length; i++) {
 	new Type(headings[i], headings[i].innerText, 100, true);
 }
