7c51dd57f7f72b921bfa1966ad0e5ab21531bc35
Timur Çelikel <me@timurcelikel.com>
2016-03-03 22:48:56 +0100

    feat(type-edit): plot list of all available characters

--- examples/type-edit.js
+++ examples/type-edit.js
@@ -7,8 +7,10 @@ require("examples/style.less");
 
 const headings = document.querySelectorAll('h2');
 
-const typeEditList = document.getElementById('typeList');
-
 for (let i = 0; i < headings.length; i++) {
 	new Type(headings[i], headings[i].innerText, 100, true);
 }
+
+const typeEditList = document.getElementById('typeList');
+
+const typeEdit = new TypeEdit(typeEditList);
--- src/type-edit.js
+++ src/type-edit.js
@@ -1,8 +1,29 @@
 import Grid from 'src/grid';
+import Type from 'src/type';
 import CHARACTERS from 'src/characters';
 const characterHeight = 4;
 
 export default class TypeEdit {
-	constructor() {
+	constructor(el) {
+		this._init(el);
+	}
+
+	_init (el) {
+		this._list = document.createElement('ul');
+		el.appendChild(this._list);
+
+		CHARACTERS.forEach((character, index) => {
+			console.log(character, index);
+			if (character) {
+				const container = document.createElement('li');
+				const heading = document.createElement('h3');
+
+				heading.innerText = index + ': ' + String.fromCharCode(index);
+
+				container.appendChild(heading);
+				new Type(container, String.fromCharCode(index), 100, true);
+				this._list.appendChild(container);
+			}
+		});
 	}
 }
