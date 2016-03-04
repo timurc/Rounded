72202c5e284f6b104ea3afe58f3900e90dd64438
Timur Çelikel <me@timurcelikel.com>
2016-03-04 10:12:20 +0100

    feat(type-editor): (space) title

--- src/type-edit.js
+++ src/type-edit.js
@@ -18,7 +18,7 @@ export default class TypeEdit {
 				const container = document.createElement('li');
 				const heading = document.createElement('h3');
 
-				heading.innerText = index + ': ' + String.fromCharCode(index);
+				heading.innerText = index + ': ' + ((index === 32) ? '(space)' : String.fromCharCode(index));
 
 				container.appendChild(heading);
 				new Type(container, String.fromCharCode(index), 100, true);
