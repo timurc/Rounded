762527fdced99a677ad250f495e22091ee0de7c3
Timur Çelikel <me@timurcelikel.com>
2016-03-16 23:01:28 +0100

    feat(type-editor) basic styling

--- src/type-edit.js
+++ src/type-edit.js
@@ -10,16 +10,23 @@ export default class TypeEdit {
 
 	_init (el) {
 		this._list = document.createElement('ul');
+		this._list.classList.add('type-edit-list');
 		el.appendChild(this._list);
 
 		CHARACTERS.forEach((character, index) => {
 			console.log(character, index);
 			if (character) {
 				const container = document.createElement('li');
+				const charCode = document.createElement('div');
 				const heading = document.createElement('h3');
 
-				heading.innerText = index + ': ' + ((index === 32) ? '(space)' : String.fromCharCode(index));
+				container.classList.add('type-edit-list-item');
+				charCode.classList.add('type-edit-char_code');
 
+				charCode.innerText = index;
+				heading.innerText = ((index === 32) ? '(space)' : String.fromCharCode(index))
+
+				container.appendChild(charCode);
 				container.appendChild(heading);
 				new Type(container, String.fromCharCode(index), 100, true);
 				this._list.appendChild(container);
