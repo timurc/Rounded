e5b90eb5c8287757c15eb6d438ef45c547498bb3
Timur Çelikel <me@timurcelikel.com>
2019-07-08 20:43:01 +0200

    migrate print stuff to separate folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,30 @@
-
 module.exports = {
     entry: {
-        main: "./examples/main.js",
-        type: "./examples/type-edit.js",
-        print: "./examples/print.js"
+        main: './examples/main.js',
+        type: './examples/type-edit.js',
+        print: './examples/print/print.js',
     },
     output: {
         path: __dirname + '/dist',
-        filename: "[name].bundle.js",
-        publicPath: '/dist'
+        filename: '[name].bundle.js',
+        publicPath: '/dist',
     },
     devtool: 'cheap-module-eval-source-map',
     resolve: {
         root: __dirname,
-        extensions: ['', '.js']
+        extensions: ['', '.js'],
     },
     module: {
         loaders: [
             {
                 test: /\.less$/,
-                loader: "style!css!less"
+                loader: 'style!css!less',
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel'
-            }
-        ]
-    }
+                loader: 'babel',
+            },
+        ],
+    },
 };
